69e54e737d87d4853f89aa07f2b504f432b08922
tianjian <1731857742@qq.com>
2017-12-14 01:27:05 +0800

    文件位置修改

---
+++ src/test/java/tianjian/junit/chapter3/section01/test/HamcrestTest.java
@@ -0,0 +1,43 @@
+package tianjian.junit.chapter3.section01.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Created by tianjian on 2017/12/14.
+ */
+public class HamcrestTest {
+    private List<String> values;
+
+    @Before
+    public void setUpList() {
+        values = new ArrayList<String>();
+        values.add("x");
+        values.add("y");
+        values.add("z");
+    }
+
+    @Test
+    public void testWithoutHamcrest() {
+        assertTrue(values.contains("one")
+                || values.contains("two")
+                || values.contains("three"));
+    }
+
+    @Test
+    public void testWithHamcrest() {
+        assertThat(values, hasItem(
+                anyOf(equalTo("one"),
+                        equalTo("two"),
+                        equalTo("three"))));
+    }
+}
--- src/test/java/tianjian/junit/chapter3/section01/test/TestDefaultController.java
+++ src/test/java/tianjian/junit/chapter3/section01/test/TestDefaultController.java
@@ -6,10 +6,12 @@ import tianjian.junit.chapter3.section01.Request;
 import tianjian.junit.chapter3.section01.RequestHandler;
 import tianjian.junit.chapter3.section01.Response;
 import tianjian.junit.chapter3.section01.impl.DefaultController;
+import tianjian.junit.chapter3.section01.impl.ErrorResponse;
 
 import static junit.framework.TestCase.assertSame;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 
 /**
  * Created by tianjian on 2017/12/11.
@@ -18,12 +20,14 @@ public class TestDefaultController {
     private DefaultController controller;
     private Request request;
     private RequestHandler handler;
+    private RequestHandler sampleExceptionHandler;
 
     @Before
     public void instantiate() {
         controller = new DefaultController();
         request = new SampleRequest();
         handler = new SampleHandler();
+        sampleExceptionHandler = new SampleExceptionHandler();
     }
 
     @Test
@@ -46,7 +50,57 @@ public class TestDefaultController {
         assertEquals("Response should be of type SampleResponse", SampleResponse.class, response.getClass());
     }
 
+    @Test
+    public void testProcessRequestAnswersErrorResponse() {
+        SampleRequest sampleRequest = new SampleRequest("testError");
+        sampleExceptionHandler = new SampleExceptionHandler();
+        controller.addHandler(sampleRequest, sampleExceptionHandler);
+        Response response = controller.processRequest(request);
+
+        assertNotNull("Must not reutn a null response", response);
+        assertEquals(ErrorResponse.class, response.getClass());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetHandlerNotDefined() {
+        SampleRequest request = new SampleRequest("testNotDefined");
+        controller.getHandler(request);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testAddRequestDuplicateName() {
+        SampleRequest request = new SampleRequest();
+        SampleHandler handler = new SampleHandler();
+        controller.addHandler(request, handler);
+    }
+
+    @Test(timeout = 130)
+    public void testProcessMultipleRequestsTimeOut() {
+        Request request;
+        Response response = new SampleResponse();
+        RequestHandler handler = new SampleHandler();
+
+        for(int i = 0; i < 99999; i++) {
+            request = new SampleRequest(String.valueOf(i));
+            controller.addHandler(request, handler);
+            response = controller.processRequest(request);
+            assertNotNull(response);
+            assertNotSame(ErrorResponse.class, response.getClass());
+        }
+    }
+
     private class SampleRequest implements Request {
+
+        private static final String DEFAULT_NAME = "Test";
+        private String name;
+
+        public SampleRequest(String name) {
+            this.name = name;
+        }
+
+        public SampleRequest() {
+            this(DEFAULT_NAME);
+        }
 
         @Override
         public String getName() {
@@ -80,5 +134,13 @@ public class TestDefaultController {
             return NAME.hashCode();
         }
 
+    }
+
+    private class SampleExceptionHandler implements RequestHandler {
+
+        @Override
+        public Response process(Request request) throws Exception {
+            throw new Exception("error processing request");
+        }
     }
 }
